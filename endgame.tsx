f248fd379f41d707c278d8e4c804b644a95935e6
jume007 <jmj2u2@gmail.com>
2024-11-03 01:17:51 -0600

    Add files via upload

---
+++ endgame.tsx
@@ -0,0 +1,144 @@
+title:Endgame Analysis
+description:https://docs.google.com/spreadsheets/d/1JM-0SlxVDAi-C6rGVlLxa-J1WGewEeL8Qvq4htWZHhY/edit?gid=346832350#gid=346832350
+
+// Perfect Paradox (god-pve)
+dimwishlist:item=817926907&perks=1482024992,1087426260,777589559,2679249093
+dimwishlist:item=817926907&perks=1482024992,1087426260,777589559,2360754333
+dimwishlist:item=817926907&perks=1482024992,1087426260,777589559,1546637391
+dimwishlist:item=817926907&perks=1482024992,1087426260,691659142,2679249093
+dimwishlist:item=817926907&perks=1482024992,1087426260,691659142,2360754333
+dimwishlist:item=817926907&perks=1482024992,1087426260,691659142,1546637391
+dimwishlist:item=817926907&perks=1482024992,1087426260,4071163871,679249093
+dimwishlist:item=817926907&perks=1482024992,1087426260,4071163871,2360754333
+dimwishlist:item=817926907&perks=1482024992,1087426260,4071163871,1546637391
+
+// Arcane Embrace (god-pve)
+dimwishlist:item=1464191693&perks=839105230,2635532173,1354429876,555281244
+dimwishlist:item=1464191693&perks=839105230,2635532173,1354429876,3708227201
+dimwishlist:item=1464191693&perks=839105230,2635532173,1354429876,2173046394
+dimwishlist:item=1464191693&perks=839105230,2635532173,1421772400,555281244
+dimwishlist:item=1464191693&perks=839105230,2635532173,1421772400,3708227201
+dimwishlist:item=1464191693&perks=839105230,2635532173,1421772400,2173046394
+
+// Veleda-F (god-pve)
+dimwishlist:item=4200122994&perks=
+
+// Sovereignty (god-pve)
+dimwishlist:item=300232691&perks=
+
+
+// Riptide (god-pve)
+
+
+// Glacioclasm (god-pve)
+
+
+// VS Velocity Baton (god-pve)
+
+
+// The Mountaintop BRAVE version (god-pve)
+
+
+// Liturgy (god-pve)
+
+
+// Martyr's Retribution (god-pve)
+
+
+// Rake Angle (god-pve)
+
+
+// Tinasha's Mastery (god-pve)
+
+
+// Indebted Kindness (god-pve)
+
+
+// VS Chill Inhibitor (god-pve)
+
+
+// Bitter/Sweet (god-pve)
+
+
+// Wicked Sister (god-pve)
+
+
+//Edge Transit BRAVE version (god-pve)
+
+
+// Falling Guillotine BRAVE Version (god-pve)
+
+
+// The Slammer (god-pve)
+
+
+// Tomorrow's Answer (god-pve)
+
+
+// Crux Termination IV (god-pve)
+
+
+// Cold Comfort (god-pve)
+
+
+// The Hothead (god-pve)
+
+
+// Herectic's Fervor (god-pve)
+
+
+// Scintillation (god-pve)
+
+
+// Mistral Lift (god-pve)
+
+
+// Stormchaser (god-pve)
+
+
+// Line in the Sand (god-pve)
+
+
+// Breakneck (god-pve)
+
+
+// Accrued Redemption (god-pve)
+
+
+// Non-Denouement (god-pve)
+
+
+// Pre Astyanax IV (god-pve)
+
+
+// Midnight Coup BRAVE Version (god-pve)
+
+
+// Warden's Law (god-pve)
+
+
+// Yesterday's Question (god-pve)
+
+
+// Blast Furnace BRAVE Version (god-pve)
+
+
+// Randy's Throwing Knift (god-pve)
+
+
+// Breechlight (god-pve)
+
+
+// Anonymous Autumn (god-pve)
+
+
+// Multimach CCX (god-pve)
+
+
+// Wilderflight (god-pve)
+
+
+// Tusk of the Boar (god-pve)
+
+
+// Ergo Sum (god-pve)
